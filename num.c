330673136e85f14c4eddd2bb23f86437e6445ff9
Arokia57 <argyaalbert.74@gmail.com>
2024-05-27 14:55:34 +0530

    new declaration

--- num.c
+++ num.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2, sum;
+    int num1, num2, hum;
     
     printf("Enter the first number: ");
     scanf("%d", &num1);
@@ -9,9 +9,9 @@ int main() {
     printf("Enter the second number: ");
     scanf("%d", &num2);
     
-    sum = num1 + num3;
+    hum = num1 + num3;
     
-    printf("The sum of %d and %d is %d.", num1, num2, sum);
+    printf("The sum of %d and %d is %d.", num1, num2, hum);
     
     return 0;
 }
